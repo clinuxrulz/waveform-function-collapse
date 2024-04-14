8308816d454a2cb62b147a2f053ef8a7760891be
Clinton Selke <clinuxrulz@gmail.com>
2024-04-14 16:03:39 +1000

    wip

--- wfc-gpujs/src/index.ts
+++ wfc-gpujs/src/index.ts
@@ -2,42 +2,61 @@ import { GPU } from "gpu.js";
 
 const gpu = new GPU();
 
-export function makePropergateFn(sourceMapRows: number, sourceMapCols: number, targetMapRows: number, targetMapColumns: number, numUniqueTiles: number): (sourceMap: number[][], targetMap: number[][]) => number[][] {
-    const propergate = gpu.createKernel(function(sourceMap: number[][], targetMap: number[][]) {
+export function makePropergateFn(sourceMapRows: number, sourceMapCols: number, targetMapRows: number, targetMapColumns: number, numUniqueTiles: number): (sourceMap: number[][], targetMap: number[][][]) => number[][][] {
+    const propergate = gpu.createKernel(function(sourceMap: number[][], targetMap: number[][][]) {
         let targetRow = this.thread.y;
         let targetCol = this.thread.x;
-        let targetVal = targetMap[targetRow][targetCol];
-        let targetUp: number;
-        if (targetRow > 0) {
-            targetUp = targetMap[targetRow - 1][targetCol];
-        } else {
-            targetUp = 0;
+        let targetTileIndex = this.thread.z;
+        let targetVal = targetMap[targetRow][targetCol][targetTileIndex];
+        if (targetVal == 0.0) {
+            return 0.0;
         }
-        let targetDown: number;
-        if (targetRow < (this.constants.targetMapRows as number) - 1) {
-            targetDown = targetMap[targetRow + 1][targetCol];
-        } else {
-            targetDown = 0;
-        }
-        let targetLeft: number;
-        if (targetCol > 0) {
-            targetLeft = targetMap[targetRow][targetCol - 1];
-        } else {
-            targetLeft = 0;
-        }
-        let targetRight: number;
-        if (targetCol < (this.constants.targetMapColumns as number) - 1) {
-            targetRight = targetMap[targetRow][targetCol + 1];
-        } else {
-            targetRight = 0;
-        }
-        let result = targetVal;
-        for (let tileIndex = numUniqueTiles-1; tileIndex >= 0; tileIndex--) {
-            
-        }
+        let result = 0.0;
         for (let sourceRow = 0; sourceRow < (this.constants.sourceMapRows as number); sourceRow++) {
             for (let sourceCol = 0; sourceCol < (this.constants.sourceMapCols as number); sourceCol++) {
-
+                let sourceVal = sourceMap[sourceRow][sourceCol];
+                if (sourceVal == targetTileIndex) {
+                    if (sourceRow > 0) {
+                        let sourceUp = sourceMap[sourceRow - 1][sourceCol];
+                        if (targetRow > 0) {
+                            if (targetMap[targetRow - 1][targetCol][sourceUp] != 0.0) {
+                                result += 1.0;
+                            }
+                        } else {
+                            result += 1.0;
+                        }
+                    }
+                    if (sourceRow < (this.constants.sourceMapRows as number) - 1) {
+                        let sourceDown = sourceMap[sourceRow + 1][sourceCol];
+                        if (targetRow < (this.constants.targetMapRows as number) - 1) {
+                            if (targetMap[targetRow + 1][targetCol][sourceDown] != 0.0) {
+                                result += 1.0;
+                            }
+                        } else {
+                            result += 1.0;
+                        }
+                    }
+                    if (sourceCol > 0) {
+                        let sourceLeft = sourceMap[sourceRow][sourceCol - 1];
+                        if (targetCol > 0) {
+                            if (targetMap[targetRow][targetCol - 1][sourceLeft] != 0.0) {
+                                result += 1.0;
+                            }
+                        } else {
+                            result += 1.0;
+                        }
+                    }
+                    if (sourceCol < (this.constants.sourceMapCols as number) - 1) {
+                        let sourceRight = sourceMap[sourceRow][sourceCol + 1];
+                        if (targetCol < (this.constants.targetMapColumns as number) - 1) {
+                            if (targetMap[targetRow][targetCol + 1][sourceRight] != 0.0) {
+                                result += 1.0;
+                            }
+                        } else {
+                            result += 1.0;
+                        }
+                    }
+                }
             }
         }
         return result;
@@ -51,5 +70,5 @@ export function makePropergateFn(sourceMapRows: number, sourceMapCols: number, t
             numUniqueTiles,
         },
     });
-    return (sourceMap: number[][], targetMap: number[][]) => propergate(sourceMap, targetMap) as number[][];
+    return (sourceMap: number[][], targetMap: number[][][]) => propergate(sourceMap, targetMap) as number[][][];
 }
