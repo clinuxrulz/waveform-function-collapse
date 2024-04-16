d68fb76db4dab5be2396b533ef7046cc38a5ad61
Clinton Selke <clinuxrulz@gmail.com>
2024-04-16 17:41:04 +1000

    wip

--- wfc-gpujs/src/index.ts
+++ wfc-gpujs/src/index.ts
@@ -7,56 +7,55 @@ const gpu = new GPU({
 
 export function makePropergateFn(sourceMapRows: number, sourceMapCols: number, targetMapRows: number, targetMapColumns: number, numUniqueTiles: number): (sourceMap: number[][], targetMap: number[][][]) => number[][][] {
     const propergate = gpu.createKernel(function(sourceMap: number[][], targetMap: number[][][]) {
-        let targetRow = this.thread.x;
-        let targetCol = this.thread.y;
-        let targetTileIndex = this.thread.z;
+        let targetRow = this.thread.y;
+        let targetCol = this.thread.z;
+        let targetTileIndex = this.thread.x;
         let targetVal = targetMap[targetRow][targetCol][targetTileIndex];
-        if (targetVal == 0.0) {
-            return 0.0;
-        }
         let result = 0.0;
-        for (let sourceRow = 0; sourceRow < (this.constants.sourceMapRows as number); sourceRow++) {
-            for (let sourceCol = 0; sourceCol < (this.constants.sourceMapCols as number); sourceCol++) {
-                let sourceVal = sourceMap[sourceRow][sourceCol];
-                if (sourceVal == targetTileIndex) {
-                    if (sourceRow > 0) {
-                        let sourceUp = sourceMap[sourceRow - 1][sourceCol];
-                        if (targetRow > 0) {
-                            if (targetMap[targetRow - 1][targetCol][sourceUp] != 0.0) {
+        if (targetVal != 0.0) {
+            for (let sourceRow = 0; sourceRow < (this.constants.sourceMapRows as number); sourceRow++) {
+                for (let sourceCol = 0; sourceCol < (this.constants.sourceMapCols as number); sourceCol++) {
+                    let sourceVal = sourceMap[sourceRow][sourceCol];
+                    if (sourceVal == targetTileIndex) {
+                        if (sourceRow > 0) {
+                            let sourceUp = sourceMap[sourceRow - 1][sourceCol];
+                            if (targetRow > 0) {
+                                if (targetMap[targetRow - 1][targetCol][sourceUp] != 0.0) {
+                                    result += 1.0;
+                                }
+                            } else {
                                 result += 1.0;
                             }
-                        } else {
-                            result += 1.0;
                         }
-                    }
-                    if (sourceRow < (this.constants.sourceMapRows as number) - 1) {
-                        let sourceDown = sourceMap[sourceRow + 1][sourceCol];
-                        if (targetRow < (this.constants.targetMapRows as number) - 1) {
-                            if (targetMap[targetRow + 1][targetCol][sourceDown] != 0.0) {
+                        if (sourceRow < (this.constants.sourceMapRows as number) - 1) {
+                            let sourceDown = sourceMap[sourceRow + 1][sourceCol];
+                            if (targetRow < (this.constants.targetMapRows as number) - 1) {
+                                if (targetMap[targetRow + 1][targetCol][sourceDown] != 0.0) {
+                                    result += 1.0;
+                                }
+                            } else {
                                 result += 1.0;
                             }
-                        } else {
-                            result += 1.0;
                         }
-                    }
-                    if (sourceCol > 0) {
-                        let sourceLeft = sourceMap[sourceRow][sourceCol - 1];
-                        if (targetCol > 0) {
-                            if (targetMap[targetRow][targetCol - 1][sourceLeft] != 0.0) {
+                        if (sourceCol > 0) {
+                            let sourceLeft = sourceMap[sourceRow][sourceCol - 1];
+                            if (targetCol > 0) {
+                                if (targetMap[targetRow][targetCol - 1][sourceLeft] != 0.0) {
+                                    result += 1.0;
+                                }
+                            } else {
                                 result += 1.0;
                             }
-                        } else {
-                            result += 1.0;
                         }
-                    }
-                    if (sourceCol < (this.constants.sourceMapCols as number) - 1) {
-                        let sourceRight = sourceMap[sourceRow][sourceCol + 1];
-                        if (targetCol < (this.constants.targetMapColumns as number) - 1) {
-                            if (targetMap[targetRow][targetCol + 1][sourceRight] != 0.0) {
+                        if (sourceCol < (this.constants.sourceMapCols as number) - 1) {
+                            let sourceRight = sourceMap[sourceRow][sourceCol + 1];
+                            if (targetCol < (this.constants.targetMapColumns as number) - 1) {
+                                if (targetMap[targetRow][targetCol + 1][sourceRight] != 0.0) {
+                                    result += 1.0;
+                                }
+                            } else {
                                 result += 1.0;
                             }
-                        } else {
-                            result += 1.0;
                         }
                     }
                 }
@@ -64,7 +63,7 @@ export function makePropergateFn(sourceMapRows: number, sourceMapCols: number, t
         }
         return result;
     }, {
-        output: [targetMapRows, targetMapColumns, numUniqueTiles],
+        output: [numUniqueTiles, targetMapColumns, targetMapRows],
         constants: {
             sourceMapRows,
             sourceMapCols,
