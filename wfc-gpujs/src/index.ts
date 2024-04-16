268e256287efef28d2a48d17c028184d03cdc110
Clinton Selke <clinuxrulz@gmail.com>
2024-04-17 09:55:27 +1000

    wip

--- wfc-gpujs/src/index.ts
+++ wfc-gpujs/src/index.ts
@@ -17,45 +17,49 @@ export function makePropergateFn(sourceMapRows: number, sourceMapCols: number, t
                 for (let sourceCol = 0; sourceCol < (this.constants.sourceMapCols as number); sourceCol++) {
                     let sourceVal = sourceMap[sourceRow][sourceCol];
                     if (sourceVal == targetTileIndex) {
+                        let match = 0;
                         if (sourceRow > 0) {
                             let sourceUp = sourceMap[sourceRow - 1][sourceCol];
                             if (targetRow > 0) {
                                 if (targetMap[targetRow - 1][targetCol][sourceUp] != 0.0) {
-                                    result += 1.0;
+                                    match += 1;
                                 }
                             } else {
-                                result += 1.0;
+                                match += 1;
                             }
                         }
                         if (sourceRow < (this.constants.sourceMapRows as number) - 1) {
                             let sourceDown = sourceMap[sourceRow + 1][sourceCol];
                             if (targetRow < (this.constants.targetMapRows as number) - 1) {
                                 if (targetMap[targetRow + 1][targetCol][sourceDown] != 0.0) {
-                                    result += 1.0;
+                                    match += 1;
                                 }
                             } else {
-                                result += 1.0;
+                                match += 1;
                             }
                         }
                         if (sourceCol > 0) {
                             let sourceLeft = sourceMap[sourceRow][sourceCol - 1];
                             if (targetCol > 0) {
                                 if (targetMap[targetRow][targetCol - 1][sourceLeft] != 0.0) {
-                                    result += 1.0;
+                                    match += 1;
                                 }
                             } else {
-                                result += 1.0;
+                                match += 1;
                             }
                         }
                         if (sourceCol < (this.constants.sourceMapCols as number) - 1) {
                             let sourceRight = sourceMap[sourceRow][sourceCol + 1];
                             if (targetCol < (this.constants.targetMapColumns as number) - 1) {
                                 if (targetMap[targetRow][targetCol + 1][sourceRight] != 0.0) {
-                                    result += 1.0;
+                                    match += 1;
                                 }
                             } else {
-                                result += 1.0;
+                                match += 1;
                             }
+                        }
+                        if (match == 4) {
+                            result += 1.0;
                         }
                     }
                 }
